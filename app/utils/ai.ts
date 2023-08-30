8a4d8ab9fb9612ab00f18f7049f0c96d172b42da
nemanjamalesija <malesija13@gmail.com>
2023-08-30 13:27:41 +0200

    Setup ai logic

--- app/utils/ai.ts
+++ app/utils/ai.ts
@@ -2,6 +2,10 @@ import { OpenAI } from 'langchain/llms/openai';
 import z from 'zod';
 import { StructuredOutputParser } from 'langchain/output_parsers';
 import { PromptTemplate } from 'langchain/prompts';
+import { Document } from 'langchain/document';
+import { loadQARefineChain } from 'langchain/chains';
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { MemoryVectorStore } from 'langchain/vectorstores/memory';
 
 const parser = StructuredOutputParser.fromZodSchema(
   z.object({
@@ -50,4 +54,24 @@ export async function analyze(content) {
   } catch (error) {
     console.log(error);
   }
+}
+
+// data about questions
+export async function qa(question, entries) {
+  const docs = entries.map((entry) => {
+    return new Document({
+      pageContent: entry.content,
+      metadata: { id: entry.id, createdAt: entry.createdAt },
+    });
+  });
+
+  const model = new OpenAI({ temperature: 0, modelName: 'gpt-3.5-turbo' });
+  const chain = loadQARefineChain(model);
+  const embeddings = new OpenAIEmbeddings();
+  const store = await MemoryVectorStore.fromDocuments(docs, embeddings);
+  const relevantDocs = store.similaritySearch(question);
+
+  const res = await chain.call({ input_documents: relevantDocs, question });
+
+  return res.output_text;
 }
